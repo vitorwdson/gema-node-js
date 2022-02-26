5af5a9b168815e8abc0b06d4e78cb3f5cec1e6f9
Vitor Wdson <vitor.wdson2@gmail.com>
2022-02-26 15:32:56 -0400

    Initial commit

---
+++ src/unary/chars-to-unary.js
@@ -0,0 +1,50 @@
+/**
+ * Write a function that takes a string as input
+ * and outputs the string in unary code
+ *
+ * Rules:
+ * - The input message consists of ASCII characters (7-bit)
+ * - The unary output message consists of blocks of 0
+ * - A block is separated from another block by a space
+ * - Two consecutive blocks are used to produce a series of same value bits (only 1 or 0 values):
+ *   - First block: it is always 0 or 00. If it is 0, then the series contains 1, if not, it contains 0
+ *   - Second block: the number of 0 in this block is the number of bits in the series
+ *
+ * Examples:
+ *
+ *   - C
+ *     charCode('C') = 67
+ *     binary(67) = '1000011'
+ *     0 0 (the first series consists of only a single 1)
+ *     00 0000 (the second series consists of four 0)
+ *     0 00 (the third consists of two 1)
+ *     So, toUnary('C') = '0 0 00 0000 0 00'
+ *
+ *   - CC
+ *     10000111000011
+ *     0 0 (one single 1)
+ *     00 0000 (four 0)
+ *     0 000 (three 1)
+ *     00 0000 (four 0)
+ *     0 00 (two 1)
+ *     So, toUnary('CC') = '0 0 00 0000 0 000 00 0000 0 00'
+ *
+ * Notes:
+ *   - All binary values must be 7-bit
+ *     E.g. '1000' !== '0001000'
+ *   - A block in a character might spill onto the next one:
+ *     E.g. '1000011' + '1000011' = '10000111000011'
+ *                ^^     ^                ^^^
+ *   - To get a string's character code use String.prototype.charCodeAt(index)
+ *     E.g. 'abc'.charCodeAt(0) = 97
+ *   - To convert a number to binary use Number.prototype.toString([radix])
+ *     E.g. (16).toString(2) = '10000'
+ *
+ */
+
+export const toUnary = chars => {
+  for (let i = 0; i < chars.length; i++) {
+    const charCode = charCode(chars.charAt(i));
+    console.log
+  // ???
+};
---
+++ src/unary/unary-to-chars.js
@@ -0,0 +1,45 @@
+/**
+ * Write a function that takes a string in unary code as input
+ * and outputs the decoded string
+ *
+ * Rules:
+ * - The input message consists of blocks of 0
+ * - The toChars output message consists of ASCII characters (7-bit)
+ * - A block is separated from another block by a space
+ * - Two consecutive blocks are used to produce a series of same value bits (only 1 or 0 values):
+ *   - First block: it is always 0 or 00. If it is 0, then the series contains 1, if not, it contains 0
+ *   - Second block: the number of 0 in this block is the number of bits in the series
+ *
+ * Examples:
+ *
+ *   0 0 00 0000 0 00
+ *   0 0 (the first series consists of only a single 1)
+ *   00 0000 (the second series consists of four 0)
+ *   0 00 (the third consists of two 1)
+ *   a single 1 + four 0 + two 1 = '1000011'
+ *   decimal('1000011') = 67
+ *   String.fromCharCode(67) = 'C'
+ *   toChars('0 0 00 0000 0 00') = 'C'
+ *
+ *   0 0 00 0000 0 000 00 0000 0 00
+ *   0 0 (one single 1)
+ *   00 0000 (four 0)
+ *   0 000 (three 1)
+ *   00 0000 (four 0)
+ *   0 00 (two 1)
+ *   '10000111000011'
+ *   7-bit split: '10000111000011' = '1000011' + '1000011'
+ *   decimal('1000011') = 67
+ *   String.fromCharCode(67) = 'C'
+ *   toChars('0 0 00 0000 0 000 00 0000 0 00') = 'CC'
+ *
+ * Notes:
+ *   - To convert a binary string to a decimal number use parseInt(string [, radix])
+ *     E.g. parseInt('1000011', 2) = 67
+ *   - To convert from a char code to a char use String.fromCharCode(num1[, ...[, numN]])
+ *     E.g. String.fromCharCode(67) = 'C'
+ */
+
+export const toChars = unary => {
+  // ???
+};
